46633e784ee4eefbb34b408334817aabe0e07822
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-04-18 22:06:52 +0200

    array indexing

---
+++ numpy_chapter/array_indexing.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+arr = np.arange(0, 11)
+print(arr)
+
+print(arr[8])
+print(arr[1:5])
+
+print(arr[:6])
+
+print(arr[-4:-1])
+
+print(arr[-1])
+slice_arr = arr[0:6]
+arr[0:5] = 100
+print(arr, slice_arr)
+
+arr_copy = arr.copy()
+
+arr_2d = np.array([[5, 10, 15], [20, 25, 30], [35, 40, 45]])
+print(arr_2d)
+
+print(arr_2d[2, 0])
+print(arr_2d[2][0])
+
+print(arr_2d[:2, 1:])
