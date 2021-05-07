0a59f75a28c3e403f2258b9bcd6981ff15d918cb
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-05-07 09:22:49 +0200

    Pretty tricky

---
+++ pandas_exercises/sf_salaries.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+sal = pd.read_csv('Salaries.csv')
+print(sal.head())
+print(sal.info())
+
+print(sal['BasePay'].mean())
+
+print(sal['OvertimePay'].max())
+
+print(sal[sal['EmployeeName'] == 'JOSEPH DRISCOLL']['JobTitle'])
+print(sal[sal['EmployeeName'] == 'JOSEPH DRISCOLL']['TotalPay'])
+
+print(sal[sal['TotalPayBenefits'] == sal['TotalPayBenefits'].min()]['EmployeeName'])
+
+print(sal.groupby('Year').mean()['BasePay'])
+
+print(sal['JobTitle'].nunique())
+
+print(sal['JobTitle'].value_counts().head(5))
+
+df2 = sal.groupby('JobTitle').count()['EmployeeName']
+
+
