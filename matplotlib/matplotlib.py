07b0399c7603a6d94252002bae861b94c8120d32
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-05-19 21:12:16 +0200

    Add matplotlib

---
+++ matplotlib/matplotlib.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# %matplotlib inline
+
+x = np.linspace(0, 5, 11)
+y = x ** 2
+print(x, y)
+plt.plot(x, y, 'r-')
+plt.show()
+
+plt.plot(x, y, 'r-')
+plt.xlabel('X Label')
+plt.ylabel('Y Label')
+plt.title('Title')
+plt.show()
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y, 'r')
+
+plt.subplot(1, 2, 2)
+plt.plot(y, x, 'b')
+plt.show()
+
+# Object oriened methods
+fig = plt.figure()
+axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+axes.plot(x, y, 'g')
+axes.set_xlabel('X Label')
+axes.set_ylabel('Y Label')
+plt.show()
+
+fig = plt.figure()
+axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+axes2 = fig.add_axes([0.2, 0.6, 0.4, 0.3])
+
+axes1.plot(x, y)
+axes2.plot(y, x)
+plt.show()
---
+++ pandas_exercises/ecommerce.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+ecom = pd.read_csv('Ecommerce Purchases')
+print(ecom.head())
+print(ecom.info())
