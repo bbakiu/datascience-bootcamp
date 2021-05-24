c42b3a3d9cc0212972327c8678e5e4138e08d642
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-05-24 22:41:36 +0200

    KNN

---
+++ 18-knn/knn.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("Classified Data")
+
+print(df.head())
+print(df.info())
+
+scaler = StandardScaler()
+scaler.fit(df.drop('TARGET CLASS', axis=1))
+scaled_features = scaler.transform(df.drop('TARGET CLASS', axis=1))
+print(scaled_features)
+
+df_feat = pd.DataFrame(scaled_features, columns=df.columns[:-1])
+print(df_feat.head())
+
+X = df_feat
+y = df['TARGET CLASS']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+knn = KNeighborsClassifier(n_neighbors=3)
+
+knn.fit(X_train, y_train)
+
+predictions = knn.predict(X_test)
+
+print(confusion_matrix(y_test, predictions))
+print(classification_report(y_test, predictions))
+
+error_rate = []
+
+for i in range(1, 40):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X_train, y_train)
+    predictions_i = knn.predict(X_test)
+    error_rate.append(np.mean(predictions_i != y_test))
+
+print(error_rate)
+plt.plot(range(1, 40), error_rate, color='blue')
+plt.show()
+
+print('======')
+knn = KNeighborsClassifier(n_neighbors=17)
+knn.fit(X_train, y_train)
+predictions = knn.predict(X_test)
+print('======')
+print(confusion_matrix(y_test, predictions))
+print('======')
+print(classification_report(y_test, predictions))
