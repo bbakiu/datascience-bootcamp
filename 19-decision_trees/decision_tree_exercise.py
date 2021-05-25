0fc3cd151a5735d75c74660b8bb48b444cbaf643
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-05-25 09:01:09 +0200

    Decision Tree

--- 19-decision_trees/decision_tree_exercise.py
+++ 19-decision_trees/decision_tree_exercise.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
 
 df = pd.read_csv('loan_data.csv')
 print(df.head())
@@ -15,20 +15,20 @@ print(df.columns)
 data_hist = df[['fico', 'credit.policy']]
 print(data_hist)
 
-plt.figure(figsize=(10,6))
-data_hist[data_hist['credit.policy']==1]['fico'].hist(alpha=0.5,color='blue',
-                                              bins=30,label='Credit.Policy=1')
-data_hist[data_hist['credit.policy']==0]['fico'].hist(alpha=0.5,color='red',
-                                              bins=30,label='Credit.Policy=0')
+plt.figure(figsize=(10, 6))
+data_hist[data_hist['credit.policy'] == 1]['fico'].hist(alpha=0.5, color='blue',
+                                                        bins=30, label='Credit.Policy=1')
+data_hist[data_hist['credit.policy'] == 0]['fico'].hist(alpha=0.5, color='red',
+                                                        bins=30, label='Credit.Policy=0')
 plt.legend()
 plt.xlabel('FICO')
 plt.show()
 
-plt.figure(figsize=(10,6))
-df[df['not.fully.paid']==1]['fico'].hist(alpha=0.5,color='blue',
-                                                      bins=30,label='Credit.Policy=1')
-df[df['not.fully.paid']==0]['fico'].hist(alpha=0.5,color='red',
-                                                      bins=30,label='Credit.Policy=0')
+plt.figure(figsize=(10, 6))
+df[df['not.fully.paid'] == 1]['fico'].hist(alpha=0.5, color='blue',
+                                           bins=30, label='Credit.Policy=1')
+df[df['not.fully.paid'] == 0]['fico'].hist(alpha=0.5, color='red',
+                                           bins=30, label='Credit.Policy=0')
 plt.legend()
 plt.xlabel('FICO')
 plt.show()
@@ -42,7 +42,7 @@ sns.lmplot(x='fico', y='int.rate', data=df, hue='credit.policy', col='not.fully.
 plt.show()
 
 cat_feats = ['purpose']
-final_data = pd.get_dummies(df,columns=cat_feats,drop_first=True)
+final_data = pd.get_dummies(df, columns=cat_feats, drop_first=True)
 
 X = final_data.drop('not.fully.paid', axis=1)
 y = df['not.fully.paid']
