438a79ef822930a6be0249d4675a0a906fc8dc3c
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-05-28 23:20:30 +0200

    NLP

--- 24-nlp/nlp.py
+++ 24-nlp/nlp.py
@@ -3,8 +3,12 @@ import string
 import matplotlib.pyplot as plt
 import pandas as pd
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
 
 # nltk.download_shell()
 messages = [line.rstrip() for line in open('data/SMSSpamCollection')]
@@ -55,7 +59,8 @@ print(clean_mess)
 def text_process(mess):
     nopunc = [char for char in mess if char not in string.punctuation]
     nopunc = ''.join(nopunc)
-    return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+    nopunc = nopunc.lower()
+    return [word for word in nopunc.split() if word not in stopwords.words('english')]
 
 
 df['tokenize'] = df['message'].apply(text_process)
@@ -65,12 +70,51 @@ bow_transformer = CountVectorizer(analyzer=text_process).fit(df['message'])
 
 print(len(bow_transformer.vocabulary_))
 mess4 = df['message'][3]
+print(df['tokenize'][3])
 print(mess4)
 
 bow4 = bow_transformer.transform([mess4])
 print(bow4)
 print(bow4.shape)
 
-bow_transformer.get_feature_names()[4068]
-bow_transformer.get_feature_names()[9554]
+# print(bow_transformer.get_feature_names()[9530])
+
+messages_bow = bow_transformer.transform(df['message'])
+
+print('Shape of sparse matrix', messages_bow.shape)
+
+print(messages_bow.nnz)
+sparsity = (100.0 * messages_bow.nnz / (messages_bow.shape[0] * messages_bow.shape[1]))
+print(('sparsity: {}'.format(sparsity)))
+
+tfidf_transformer = TfidfTransformer().fit(messages_bow)
+
+tfidf4 = tfidf_transformer.transform(bow4)
+print(tfidf4)
+
+
+print(tfidf_transformer.idf_[bow_transformer.vocabulary_['university']])
+
+messages_tfidf = tfidf_transformer.transform(messages_bow)
+
+spam_detect_model = MultinomialNB().fit(messages_tfidf,df['label'])
+
+print(spam_detect_model.predict(tfidf4)[0])
+
+all_pred = spam_detect_model.predict(messages_tfidf)
+print(all_pred)
+
+msg_train, msg_test, label_train, label_test = train_test_split(df['message'], df['label'], test_size=0.3)
+
+pipeline = Pipeline([
+    ('bow', CountVectorizer(analyzer=text_process)),
+    ('tfidf', TfidfTransformer()),
+    ('classifier', MultinomialNB())
+])
+
+pipeline.fit(msg_train, label_train)
+
+predictions = pipeline.predict(msg_test)
+
+print(classification_report(label_test, predictions))
 
