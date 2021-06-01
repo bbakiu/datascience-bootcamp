0f14e9a71e60a7329e9b457e96b791fff4e180ea
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-06-01 09:03:15 +0200

    Keras Regression

--- 25-deep_learning/tf_regression.py
+++ 25-deep_learning/tf_regression.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
 
 df = pd.read_csv('data/kc_house_data.csv')
 
@@ -61,5 +67,44 @@ plt.show()
 
 df.drop('date', axis=1, inplace=True)
 
+df['zipcode'].value_counts()
 
+df.drop('zipcode', axis=1, inplace=True)
+# df.drop('yr_renovated', axis=1, inplace=True)
 
+X = df.drop('price', axis=1).values
+y = df['price'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = Sequential()
+
+model.add(Dense(19, activation='relu'))
+model.add(Dense(19, activation='relu'))
+model.add(Dense(19, activation='relu'))
+model.add(Dense(19, activation='relu'))
+
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+model.fit(X_train, y_train, epochs=400, validation_data=(X_test, y_test), batch_size=128)
+
+losses = pd.DataFrame(model.history.history)
+losses.plot()
+
+predictions = model.predict(X_test)
+
+print(np.sqrt(mean_squared_error(y_test, predictions)))
+print(mean_absolute_error(y_test, predictions))
+
+print(df['price'].describe())
+
+print(explained_variance_score(y_test, predictions))
+
+singlehouse = df.drop('price', axis=1).iloc[0]
+singlehouse = scaler.transform(singlehouse.values.reshape(-1, 19))
+
+print(model.predict(singlehouse))
