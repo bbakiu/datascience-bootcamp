3319d92a7d05c5483ee9c64525785ffe3a0d2244
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-06-01 08:31:06 +0200

    Keras Regression

--- 25-deep_learning/tf_regression.py
+++ 25-deep_learning/tf_regression.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
 
 df = pd.read_csv('data/kc_house_data.csv')
 
@@ -14,3 +12,42 @@ print(df.isnull().sum())
 
 sns.displot(df['price'])
 plt.show()
+
+sns.countplot(df['bedrooms'])
+plt.show()
+
+print(df.corr()['price'].sort_values())
+
+sns.scatterplot(x='price', y='sqft_living', data=df)
+plt.figure(figsize=(12, 6))
+plt.show()
+
+sns.boxplot(x='bedrooms', y='price', data=df)
+plt.show()
+
+sns.scatterplot(x='price', y='long', data=df)
+plt.figure(figsize=(12, 8))
+plt.show()
+
+sns.scatterplot(x='price', y='lat', data=df)
+plt.figure(figsize=(12, 8))
+plt.show()
+
+sns.scatterplot(x='long', y='lat', data=df, hue='price')
+plt.figure(figsize=(12, 8))
+plt.show()
+
+print(df.sort_values('price', ascending=False).head(215))
+
+bottom_99 = df.sort_values('price', ascending=False).iloc[216:]
+
+sns.scatterplot(x='long', y='lat', data=bottom_99, hue='price', edgecolor=None, alpha=0.2, palette='RdYlGn')
+plt.figure(figsize=(12, 8))
+plt.show()
+
+sns.boxplot(x='waterfront', y='price', data=df)
+
+df.drop('id', axis=1, inplace=True)
+df['date'] = pd.to_datetime(df['date'])
+df['year'] = df['date'].apply(lambda date: date.year)
+df['month'] = df['date'].apply(lambda date: date.month)
