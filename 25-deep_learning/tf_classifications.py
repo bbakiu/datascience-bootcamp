19d05b5f4a46d5e5416e95d4a29bf9996b04eeef
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-06-02 07:59:33 +0200

    Keras Classification

--- 25-deep_learning/tf_classifications.py
+++ 25-deep_learning/tf_classifications.py
@@ -4,6 +4,12 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import  EarlyStopping
+
+from sklearn.metrics import classification_report, confusion_matrix
+
 df = pd.read_csv('data/cancer_classification.csv')
 
 print(df.head())
@@ -30,3 +36,55 @@ scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
+model = Sequential()
+
+model.add(Dense(30, activation='relu'))
+model.add(Dense(15, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam')
+
+model.fit(x=X_train, y = y_train, epochs=600, validation_data=(X_test, y_test))
+
+lossed = pd.DataFrame(model.history.history)
+lossed.plot()
+plt.show()
+
+
+model2 = Sequential()
+
+model2.add(Dense(30, activation='relu'))
+model2.add(Dense(15, activation='relu'))
+model2.add(Dense(1, activation='sigmoid'))
+
+model2.compile(loss='binary_crossentropy', optimizer='adam')
+
+early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=25)
+model2.fit(x=X_train, y = y_train, epochs=600, validation_data=(X_test, y_test), callbacks=[early_stopping])
+
+lossed2 = pd.DataFrame(model2.history.history)
+lossed2.plot()
+plt.show()
+
+
+model3 = Sequential()
+
+model3.add(Dense(30, activation='relu'))
+model3.add(Dropout(0.5))
+model3.add(Dense(15, activation='relu'))
+model3.add(Dropout(0.5))
+model3.add(Dense(1, activation='sigmoid'))
+
+model3.compile(loss='binary_crossentropy', optimizer='adam')
+
+early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=25)
+model3.fit(x=X_train, y = y_train, epochs=600, validation_data=(X_test, y_test), callbacks=[early_stopping])
+
+lossed3 = pd.DataFrame(model3.history.history)
+lossed3.plot()
+plt.show()
+
+predictions = model3.predict_classes(X_test)
+
+print(classification_report(y_test, predictions))
+print(confusion_matrix(y_test, predictions))
